987432b95dfc3554cfe0a6efdf55d7f0ec6808a0
Eric Young <ecyoung@gmail.com>
2021-07-26 20:41:23 +0800

    Create bubble_sort.py

---
+++ sorting_searching/bubble_sort.py
@@ -0,0 +1,18 @@
+"""
+bubble_sort.py
+"""
+
+def bubble_sort(arr):
+    # for every element (arranged backwards)
+    for n in range(len(arr)-1,0,-1):
+        for k in range(n):
+            # if we come to a point to switch 
+            if arr[k] > arr[k+1]:
+                temp = arr[k]
+                arr[k] = arr[k+1]
+                arr[k+1] = temp
+
+arr = [3,2,13,4,6,5,7,8,1,20]
+bubble_sort(arr)
+print(arr)
+            
